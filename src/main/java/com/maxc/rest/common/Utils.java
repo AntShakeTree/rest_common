1ef1e842ef7877e60d44920643c8f4632fe04fb7
ant_shake_tree <ant_shake_tree@163.com>
2015-08-24 16:07:41 +0800

    xxx

--- src/main/java/com/maxc/rest/common/Utils.java
+++ src/main/java/com/maxc/rest/common/Utils.java
@@ -1,5 +1,9 @@
 package com.maxc.rest.common;
 
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -12,6 +16,7 @@ import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.beanutils.BeanComparator;
@@ -378,5 +383,64 @@ public class Utils {
 		date.set(13, 0);
 		date.set(14, 0);
 		return date.getTimeInMillis();
+	}
+
+
+	public static String MD5(String md5Str) {
+		MessageDigest md5;
+		StringBuffer sb = new StringBuffer();
+		try {
+			md5 = MessageDigest.getInstance("MD5");
+			md5.reset();
+			BigInteger pwInt = new BigInteger(1, md5.digest(md5Str.getBytes()));
+
+			String pwStr = pwInt.toString(16);
+			int padding = 32 - pwStr.length();
+			for (int i = 0; i < padding; i++) {
+				sb.append('0');
+			}
+			sb.append(pwStr);
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			return "";
+		}
+		return sb.toString();
+	}
+
+	/**
+	 *
+	 * @param @param ip
+	 * @param @return
+	 * @return boolean
+	 * @throws
+	 * @since : v1.0.0.0
+	 */
+	public static boolean isInetAddress(String ip) {
+		String reg = "\\b((?!\\d\\d\\d)\\d+|1\\d\\d|2[0-4]\\d|25[0-5])\\.((?!\\d\\d\\d)\\d+|1\\d\\d|2[0-4]\\d|25[0-5])\\.((?!\\d\\d\\d)\\d+|1\\d\\d|2[0-4]\\d|25[0-5])\\.((?!\\d\\d\\d)\\d+|1\\d\\d|2[0-4]\\d|25[0-5])\\b";
+		if (!matches(ip, reg)) {
+			try {
+				String add = InetAddress.getByName(ip).getHostAddress();
+				return matches(add, reg);
+			} catch (Exception e) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	/**
+	 * Title: matches : d
+	 *
+	 * @param value
+	 * @param regular
+	 * @param
+	 * @return boolean
+	 * @throws
+	 */
+	private static boolean matches(String value, String regular) {
+		Pattern pattern = Pattern.compile(regular);
+		Matcher matcher = pattern.matcher(value); // 娴犮儵鐛欑拠锟�7.400.600.2娑撹桨绶�
+		return matcher.matches();
 	}
 }
